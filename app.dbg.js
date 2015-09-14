d00cb392248f8ad2df345bfbf5f820fb4e62eb49
Thomas Stringer <sqlsalt@outlook.com>
2015-09-14 14:41:16 -0400

    Changed route logic

--- app.dbg.js
+++ app.dbg.js
@@ -1,7 +1,6 @@
-var content = require('./data/content.js');
+var content = require('./data/content');
 
 console.log('about to retrieve top question...');
 content.getCurrentQuestion(function (currentQuestion) {
     console.log('and the top question is... ' + currentQuestion.text);
 });
-console.log('done...');
--- routes/questions.js
+++ routes/questions.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var content = require('../data/content');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.status(200).json({
-        message: "testing here"
+    content.getCurrentQuestion(function (currentQuestion) {
+        res.status(200).json({
+            message: currentQuestion.text
+        });
     });
 });
 
