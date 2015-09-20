302d579746aa2655df5bc650d4dc61493ad3c5dd
Thomas Stringer <sqlsalt@outlook.com>
2015-09-19 21:06:45 -0400

    Added empty answers arrays to the seed documents

--- data/mongo-dbg-data-seed.js
+++ data/mongo-dbg-data-seed.js
@@ -14,42 +14,48 @@ var documents = [
         "upVotes" : 153,
         "downVotes" : 16,
         "isCurrent" : false,
-        "isNextPossibility" : true
+        "isNextPossibility" : true,
+        "answers": []
     },
     {
         "text" : "what color is a lion?",
         "upVotes" : 23,
         "downVotes" : 4,
         "isCurrent" : true,
-        "isNextPossibility" : false
+        "isNextPossibility" : false,
+        "answers": []
     },
     {
         "text" : "do you think we'll have flying cars in 20 years?",
         "upVotes" : 167,
         "downVotes" : 236,
         "isCurrent" : false,
-        "isNextPossibility" : true
+        "isNextPossibility" : true,
+        "answers": []
     },
     {
         "text" : "how many gallons of water does the ocean have?",
         "upVotes" : 53,
         "downVotes" : 24,
         "isCurrent" : false,
-        "isNextPossibility" : true
+        "isNextPossibility" : true,
+        "answers": []
     },
     {
         "text" : "what is the best city to live in?",
         "upVotes" : 1523,
         "downVotes" : 62,
         "isCurrent" : false,
-        "isNextPossibility" : false
+        "isNextPossibility" : false,
+        "answers": []
     },
     {
         "text" : "who sells the best cheeseburger?",
         "upVotes" : 12629,
         "downVotes" : 4,
         "isCurrent" : false,
-        "isNextPossibility" : false
+        "isNextPossibility" : false,
+        "answers": []
     }
 ];
 db.questions.insert(documents);
