589377e61b876958cbff559c75c750fef2531ce9
Thomas Stringer <sqlsalt@outlook.com>
2015-10-04 20:37:19 -0400

    Created logic to dynamically add a new answer

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,3 +1,33 @@
+// ********************************************************
+//                  UI helpers
+// ********************************************************
+
+function generateAnswerBox(answerText, upVoteCount, downVoteCount) {
+    return "<div class='col-md-12 answer voting-item'>" +
+            "<div class='col-md-1'>" +
+                "<span class='glyphicon glyphicon-chevron-up vote-button'></span>" +
+                "<span class='glyphicon glyphicon-chevron-down vote-button'></span>" +
+            "</div>" +
+            "<div class='col-md-11'>" +
+                "<div class='col-md-11 votes'>" +
+                    "<div class='col-md-3'>" +
+                        "<p class='up-votes'>" + upVoteCount + "</p>" +
+                    "</div>" +
+                    "<div class='col-md-3'>" +
+                        "<p class='down-votes'>" + downVoteCount + "</p>" +
+                    "</div>" +
+                "</div>" +
+                "<div class='col-md-11'>" +
+                    "<p>" + answerText + "</p>" +
+                "</div>" +
+            "</div>" +
+        "</div>";
+}
+
+// ********************************************************
+//                  initial load
+// ********************************************************
+
 function initialLoadActions() {
     
 }
