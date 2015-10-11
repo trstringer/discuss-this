9fe9e369fb6d78ac566c2534089640ef7aac40eb
Thomas Stringer <sqlsalt@outlook.com>
2015-10-11 13:38:09 -0400

    Adding worker functions to generate answers and questions

--- testing/input-generator.js
+++ testing/input-generator.js
@@ -1,14 +1,33 @@
 var http = require('http');
+var words = ["blue", "green", "yellow", "red", "purple", "pink", "lion", "horse", "shark", "dolphin"];
 
-http.get(
-    "http://localhost:3000/questions/",
-    function (res) {
-        var responseData = '';
-        res.on('data', function (data) {
-            responseData += data;
-        });
-        res.on('end', function () {
-            console.log(JSON.parse(responseData));
-        });
+function getRandomWord() {
+    return words[Math.floor(Math.random(0, words.length) * words.length)];
+}
+function getMetaString() {
+    return (new Date()).getTime();
+}
+function getRandomAnswer() {
+    var answer = "ANS " + getMetaString();
+    while (answer.length < 40) {
+        answer += " " + getRandomWord();
     }
-);
+    return answer + ".";
+}
+function getRandomQuestion() {
+    var question = "QST " + getMetaString();
+    while (question.length < 40) {
+        question += " " + getRandomWord();
+    }
+    return question + "?";
+}
+
+setInterval(function () {
+    console.log(getRandomQuestion());
+}, 1000);
+
+setTimeout(function() {
+    setInterval(function () {
+        console.log(getRandomAnswer());
+    }, 1000);
+}, 500);
